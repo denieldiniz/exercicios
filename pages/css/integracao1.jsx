db80a8eb84bf91c4cef83180210df06af4c7bc08
denieldiniz <denieldiniz@gmail.com>
2023-09-27 06:49:57 -0300

    inicio css

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,11 @@
+import "@/pages/_app.js"
+
+export default function integracao() {
+  return (
+    <>
+      <div id="vermelha">Texto #01</div>
+      <div id="azul">Texto #02</div>
+      <div id="branco">Texto #03</div>
+    </>
+  )
+}
