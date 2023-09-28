0fda4eabe90fb02b7d4500da7e5399db596a395b
denieldiniz <denieldiniz@gmail.com>
2023-09-28 07:44:08 -0300

    css aula 1

--- pages/css/integracao1.jsx
+++ pages/css/integracao1.jsx
@@ -3,9 +3,9 @@ import "@/pages/_app.js"
 export default function integracao() {
   return (
     <>
-      <div id="vermelha">Texto #01</div>
-      <div id="azul">Texto #02</div>
-      <div id="branco">Texto #03</div>
+      <div className="vermelho">Texto #01</div>
+      <div className="azul">Texto #02</div>
+      <div className="branco">Texto #03</div>
     </>
   )
 }
